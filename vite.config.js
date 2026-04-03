c68554e5af31a0e27a337e06c1d3038ff8ea92cd
perfect1qq <101235247+perfect1qq@users.noreply.github.com>
2026-04-03 13:02:41 +0800

    更改了仓库名称

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   // 💡 添加下面这一行！
   // 格式是 '/仓库名字/'，记得前  后都有斜杠
   // 如果你刚才在 GitHub 建的仓库叫 my-vue-app，这里就写 '/my-vue-app/'
-  base: '/vue-project/',
+  base: '/Beilit-Price-List/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
