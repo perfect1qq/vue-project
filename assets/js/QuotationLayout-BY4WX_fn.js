c5bb3b8de4b0e05c12b22880f0a61f14e7e85840
perfect1qq <101235247+perfect1qq@users.noreply.github.com>
2026-04-28 00:58:57 +0000

    Deploying to gh-pages from @ perfect1qq/Beilit-Price-List@1ba787ef2a6365846212c0ab77c0d9fb92c83fa1 🚀

--- assets/js/QuotationLayout-BY4WX_fn.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./index-CBbshS-F.js";import{at as r,L as e,M as n}from"./vendor-vue-CYxUtjpk.js";import"./vendor-element-plus-QfyJkSgg.js";import"./vendor-misc-zxlWNkOO.js";import"./vendor-axios-42ANG6Sg.js";const c={};function a(s,_){const o=r("router-view");return e(),n(o)}const l=t(c,[["render",a]]);export{l as default};
